f47c2040ac2af8b0de973760ad2b4b96c0b2179f
clarice_r <claricerdallorto@gmail.com>
2021-05-11 17:42:23 +0100

    Added basic functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-
+const areaValue = document.querySelector("#area-value");
+const setMin = document.querySelector("#min-value");
+const setStep = document.querySelector("#step-value");
+const calculateButton = document.querySelector("#calc-btn");
+const combinationsList = document.querySelector(".comb-list");
 
 // Area Combinations Calculator
 function areaCombinations(area, setMin = 1, setStep = 1) {
@@ -22,4 +26,27 @@ function formatCombinations(nestedArray) {
         formatedArray.push(array.join(' x '));
     }
     return formatedArray;
+}
+
+// Click event for button
+calculateButton.addEventListener("click", () => {
+    const area = areaValue.value;
+    // const setMin = setMin.value;
+    // const setStep = setStep.value;
+    const combArray = areaCombinations(area);
+    const strCombArray = formatCombinations(combArray);
+    for (let pair of strCombArray) {
+        let li = document.createElement('li');
+        li.innerText = pair;
+        combinationsList.appendChild(li);
+    }
+    combinationsList.classList.remove('hide');
+    clearInput();
+})
+
+// clear and reset form
+const clearInput = () => {
+    areaValue.value = '';
+    setMin.value = '';
+    setStep.value = '';
 }
