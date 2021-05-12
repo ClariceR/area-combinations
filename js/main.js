483d58bc979f481443ceec88077a20e99dd63e41
clarice_r <claricerdallorto@gmail.com>
2021-05-12 14:36:13 +0100

    Clicking the button now shows results in cards instead of a list

--- js/main.js
+++ js/main.js
@@ -12,11 +12,26 @@ function areaCombinations(area, setMin, setStep) {
         height = i;
         combination.push(height);
         let width = area / height;
+        console.log(width);
+        if (isDecimal(area, height)) {
+            width = limitDecimalPlaces(width);
+        }
+        console.log(width);
         combination.push(width);
         allCombinations.push(combination);
         combination = [];
     }
     return allCombinations;
+}
+
+// Check if number is decimal
+function isDecimal(area, height) {
+    return area % height !== 0;
+}
+
+// Deals with decimal numbers
+function limitDecimalPlaces(number) {
+        return number = parseFloat(number).toFixed(4);
 }
 
 // Format the array's areas into strings separated by 'x'
@@ -28,28 +43,55 @@ function formatCombinations(nestedArray) {
     return formatedArray;
 }
 
-// Click event for button
+// Click event for button - working!
 calculateButton.addEventListener("click", () => {
+    addCard();
+    clearInput();
+})
+
+//add pairs to list
+const addPairs = () => {
     combinationsList.innerHTML = '';
     let areaValue = parseFloat(area.value);
     let setMinValue = parseFloat(setMin.value);
     let setStepValue = parseFloat(setStep.value);
     const combArray = areaCombinations(areaValue, setMinValue, setStepValue);
-    console.log(combArray);
     const strCombArray = formatCombinations(combArray);
-    console.log(strCombArray);
     for (let pair of strCombArray) {
         let li = document.createElement('li');
         li.innerText = pair;
         combinationsList.appendChild(li);
     }
     combinationsList.classList.remove('hide');
-    // clearInput();
-})
+}
+
+// add card
+const addCard = () => {
+    combinationsList.innerHTML = '';
+    let areaValue = parseFloat(area.value);
+    let setMinValue = parseFloat(setMin.value);
+    let setStepValue = parseFloat(setStep.value);
+    const combArray = areaCombinations(areaValue, setMinValue, setStepValue);
+    const strCombArray = formatCombinations(combArray);
+    for (let pair of strCombArray) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add("col");
+        cardDiv.classList.add("s6");
+        cardDiv.classList.add("m5");
+        cardDiv.innerHTML = `
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text center">
+                <p>${pair}</p>
+              </div>
+            </div>`;
+        combinationsList.append(cardDiv);
+    }
+    combinationsList.classList.remove('hide')
+}
 
 // clear and reset form
-// const clearInput = () => {
-//     areaValue.value = '';
-//     setMinValue.value = 1;
-//     setStepValue.value = 1;
-// }
+const clearInput = () => {
+    area.value = '';
+    setMin.value = 1;
+    setStep.value = 1;
+}
